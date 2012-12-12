871afea6d2adcfc160b5dd404b76ff4d4bc4f4ae
rafaeldx7 <rafaeldx7@gmail.com>
2012-12-12 18:38:52 -0200

    Fix ex17.c.

--- ex17.c
+++ ex17.c
@@ -36,8 +36,9 @@ void die(const char *message)
 
 void Address_print(struct Address *addr)
 {
-  printf("%d %s\n", addr->id, addr->name, addr->email);
+  printf("%d %s %s\n", addr->id, addr->name, addr->email);
 }
+
 
 void Database_load(struct Connection *conn)
 {
@@ -83,6 +84,9 @@ void Database_write(struct Connection *conn)
 
   int rc = fwrite(conn->db, sizeof(struct Database), 1, conn->file);
   if (rc != 1) die("Failed to write database.");
+
+  rc = fflush(conn->file);
+  if (rc == -1) die("Cannot flush database.");
 }
 
 void Database_create(struct Connection *conn)
@@ -105,6 +109,7 @@ void Database_set(struct Connection *conn, int id, const char *name, const char 
   addr->set = 1;
   // WARNING: bug, read the "How to Break It" and fix this
   char *res = strncpy(addr->name, name, MAX_DATA);
+  // demonstrate the strncpy bug
   if (!res) die("Name copy failed.");
 
   res = strncpy(addr->email, email, MAX_DATA);
@@ -183,7 +188,6 @@ int main(int argc, char *argv[])
     case '1':
       Database_list(conn);
       break;
-
     default:
       die("Invalid action, only: c = create, g = get, s = set, d = delete, l = list");
   }
